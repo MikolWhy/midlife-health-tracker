8bea4ed7769cd6efd3f67af56f5df06e173010f4
Michael Yik <Michael.yik7@gmail.com>
2026-01-29 03:37:36 -0500

    chore(frontend): add Tailwind theme, path aliases, and env setup, basic index.html

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' //add path alias to vite config
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
